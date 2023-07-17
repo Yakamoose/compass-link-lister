bfc55e8453c0a7592d2641f29287a1d16b198d59
Yakamoose <xericx.johnson@gmail.com>
2023-07-17 10:08:47 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'compass-link-lister';
+export class AppComponent implements OnInit {
+  productData!: any[];
+  productList!: any[];
+
+  constructor(private http: HttpClient) {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get('./assets/productData-july16.json').subscribe((data: any) => {
+      this.productData = data.data;
+      // console.log(this.productData);
+
+      this.productList = this.listConverter(this.productData);
+      // call display func here with (this.data)
+      this.displayUI(this.productList);
+    });
+  }
+
+  listConverter(productData: any[]): any[] {
+    // sort alphabetically    
+    productData.sort((a, b) => a.productName.localeCompare(b.productName));
+    productData.sort((a, b) => a.portfolio.localeCompare(b.portfolio));
+
+    productData = productData.filter(({ recordType }) => !['Family'].includes(recordType))
+
+    // remove recordType = Family
+    return productData;
+  }
+
+  displayUI(productList: any[]) {
+    console.log({ productList })
+  }
+
+  ngOnInit() {
+    // Fetch JSON data from assets
+
+  }
+
 }
